9751f5bcb4470dd99607b3d423262aa132bab390
zhd1987e <zhanghaodong101@outlook.com>
2026-04-01 12:52:29 +0800

    Removed need for actions AND reformatted stuff

--- convertMASTERDATA.py
+++ convertMASTERDATA.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 data = pd.read_csv("MASTERDATACSV.csv")
-data["AwardingTitle"] = data["Courses"] + " " + data["Title"] + f" (Chair: {data["Instructor"]})"
+data["AwardingTitle"] = data["Courses"] + " " + data["Title"] + " (Chair: " + data["Instructor"] + ")"
 data.set_index("Courses", inplace=True)
 data.to_json("28th-steps-awardData.dat", orient="index", indent=4)
